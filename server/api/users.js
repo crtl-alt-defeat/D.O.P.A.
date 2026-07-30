00c26cba1364a0c5ab1f7181092dd3b33e870b1a
Paige Cotignola <hello.paigenola@gmail.com>
2026-07-30 11:54:51 -0500

    added Users Queries

--- server/api/users.js
+++ server/api/users.js
@@ -1,8 +1,32 @@
 import express from "express";
 const usersRouter = express.Router();
+import { createUser, getUserById } from "../db/usersQueries.js";
 
-usersRouter.get("/", async (req, res, next) => {
-  res.send("inside of GET /api/users route!");
+usersRouter.get("/:id", async (req, res, next) => {
+  try {
+    const user = await getUserById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    res.send(user);
+  } catch (error) {
+    next(error); // sends to custom error handler is server/index.js <- should I do this?
+  }
+});
+
+usersRouter.post("/register", async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).send({ message: "Missing required field(s)" });
+    }
+
+    const newUser = await createUser({ username, email, password });
+    res.status(201).send(newUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default usersRouter;
--- server/db/seed.js
+++ server/db/seed.js
@@ -1,7 +1,21 @@
 import client from "./client.js";
 
 const seed = async () => {
-  console.log("add logic to create and seed tables");
+  try {
+    console.log("Making your table girl..");
+
+    await client.query(`
+    CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL
+    password TEXT NOT NULL
+    );
+    `);
+    console.log("Table built! Woot!");
+  } catch (error) {
+    console.error("Boo! Error seeding database:", error);
+  }
 };
 
 export default seed;
