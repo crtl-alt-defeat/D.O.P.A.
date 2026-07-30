6a5bfbe1d0e67ccdc52e8b2ea83e3156a3d9103b
Corynne Ikola <cory@ikola.xyz>
2026-07-30 11:35:14 -0500

    getTypes,getType, getTypesByUserId, getGoals,getGoal, and getGoalsByUserId added.

--- server/db/goals.js
+++ server/db/goals.js
@@ -1,10 +1,41 @@
 import client from "../db/client.js";
-export async function createGoal(name, type_id) {
+
+export async function createGoal(name, goal_id) {
   const SQL = `
     INSERT INTO goals (name, type_id)
     VALUES ($1, $2)
     RETURNING *
     `;
-  const reponse = await client.query(SQL, [name, type_id]);
+  const response = await client.query(SQL, [name, goal_id]);
   return response.rows[0];
+}
+export async function getGoals() {
+  const SQL = `
+  SELECT *
+  FROM goals
+  `;
+  const { rows: goals } = await client.query(SQL);
+  return goals;
+}
+export async function getGoal(id) {
+  const SQL = `
+  SELECT *
+  FROM goals
+  WHERE id = $1
+  `;
+  const {
+    rows: [goal],
+  } = await client.query(SQL, [id]);
+  return goal;
+}
+
+export async function getGoalsByUserId(userId) {
+  const SQL = `
+  SELECT goals.*
+  FROM goals
+  JOIN users_goals ON users_goals.goal_id = goals.id
+  WHERE users_goals.user_id = $1
+  `;
+  const { rows: goals } = await client.query(SQL, [userId]);
+  return goals;
 }
--- server/db/types.js
+++ server/db/types.js
@@ -1,10 +1,40 @@
 import client from "../db/client.js";
 export async function createType(name) {
   const SQL = `
-    INSERT INTO goals (name)
+    INSERT INTO types (name)
     VALUES ($1)
     RETURNING *
     `;
-  const reponse = await client.query(SQL, [name]);
+  const response = await client.query(SQL, [name]);
   return response.rows[0];
+}
+export async function getTypes() {
+  const SQL = `
+  SELECT *
+  FROM types
+  `;
+  const { rows: types } = await client.query(SQL);
+  return types;
+}
+export async function getType(id) {
+  const SQL = `
+  SELECT *
+  FROM types
+  WHERE id = $1
+  `;
+  const {
+    rows: [type],
+  } = await client.query(SQL, [id]);
+  return type;
+}
+
+export async function getTypesByUserId(userId) {
+  const SQL = `
+  SELECT types.*
+  FROM types
+  JOIN users_types ON users_types.type_id = types.id
+  WHERE users_types.user_id = $1
+  `;
+  const { rows: types } = await client.query(SQL, [userId]);
+  return types;
 }
