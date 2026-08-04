9ab76366b0c7a77bfdd6efb2afa428056a2fdbfc
Raven Bratcher <ravenbratcher@gmail.com>
2026-08-04 12:32:10 -0500

    did all that, some tests may be broken lmao

--- server/api/users.js
+++ server/api/users.js
@@ -2,12 +2,48 @@ import express from "express";
 const usersRouter = express.Router();
 
 //middleware
-//todo: import requireBody (copy middleware folder from previous assignments)
-//todo: import getUserFromToken (copy middleware folder from previous assignments)
-//todo: import requireUser (copy middleware folder from previous assignments)
+import requireBody from "../middleware/requireBody.js";
+import getUserFromToken from "../middleware/getUsersFromToken.js";
+import requireUser from "../middleware/requireUser.js";
 
 //queries
-import { createUser, getUserById } from "../db/queries/users.js";
+import { authenticate, createUser, getUserById } from "../db/queries/users.js";
+
+usersRouter.post(
+  "/register",
+  requireBody(["name", "email", "password"]),
+  async (req, res, next) => {
+    try {
+      const newUser = req.body;
+      const token = await createUser(newUser);
+      res.status(201).send(token);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+usersRouter.post(
+  "/login",
+  requireBody(["email", "password"]),
+  async (req, res) => {
+    const userInfo = req.body;
+    const token = await authenticate(userInfo);
+    res.send(token);
+  },
+);
+
+//todo: make a 'GET /me' route, that requires a token and returns a user
+usersRouter.get("/me", getUserFromToken, requireUser, async (req, res) => {
+  console.log(req);
+  res.send(req.user);
+});
+
+//todo: move 'GET /types/user/:userId' to here as 'GET /users/me/types
+//todo:   requiring a token and returning a list of types
+
+//todo: move 'GET /goals/user/:userId' to here as 'GET /users/me/goals
+//todo:   requiring a token and returning a list of goals
 
 usersRouter.get("/:id", async (req, res, next) => {
   //todo: make more secure maybe? (require token and restrict to admins? [this would require implementing an admin role in users])
@@ -21,31 +57,5 @@ usersRouter.get("/:id", async (req, res, next) => {
     next(error); // sends to custom error handler is server/index.js <- should I do this?
   }
 });
-
-//todo: use middleware (requireBody)
-usersRouter.post("/register", async (req, res, next) => {
-  try {
-    const { username, email, password } = req.body;
-
-    if (!username || !email || !password) {
-      return res.status(400).send({ message: "Missing required field(s)" });
-    }
-
-    const newUser = await createUser({ username, email, password });
-    res.status(201).send(newUser);
-  } catch (error) {
-    next(error);
-  }
-});
-
-//todo: make a 'POST /login' route
-
-//todo: make a 'GET /me' route, that requires a token and returns a user
-
-//todo: move 'GET /types/user/:userId' to here as 'GET /users/me/types
-//todo:   requiring a token and returning a list of types
-
-//todo: move 'GET /goals/user/:userId' to here as 'GET /users/me/goals
-//todo:   requiring a token and returning a list of goals
 
 export default usersRouter;
